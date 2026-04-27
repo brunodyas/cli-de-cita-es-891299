a26b630f119cf6a7d82266486901372d3eb03da4
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 10:56:35 -0300

    chore: projeto console .NET 8
    intent: chore

--- obj/Debug/net8.0/cli_de_cita_es_891299.AssemblyInfo.cs
+++ obj/Debug/net8.0/cli_de_cita_es_891299.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cli_de_cita_es_891299")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a36182aaa48e992efc06bd64ec0b277db86c7e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f23ba047da88808b36ef29a4aa74d6301b52df")]
 [assembly: System.Reflection.AssemblyProductAttribute("cli_de_cita_es_891299")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cli_de_cita_es_891299")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
